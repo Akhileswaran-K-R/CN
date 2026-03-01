6f00f3f818f055186ef251bc78e2a32557e36d64
Akhileswaran-K-R <akhilarathi956@gmail.com>
2026-03-02 02:42:25 +0530

    done ftp

---
+++ TCP/FTP/client.c
@@ -0,0 +1,34 @@
+#include<stdio.h>
+#include<stdlib.h>
+#include<string.h>
+#include<sys/socket.h>
+#include<sys/types.h>
+#include<netinet/in.h>
+#include<arpa/inet.h>
+#include<unistd.h>
+
+void main(){
+  struct sockaddr_in serverAddr;
+  char buffer[1024];
+  
+  int clientSocket = socket(AF_INET,SOCK_STREAM,0);
+  printf("Client socket created successfully\n");
+
+  serverAddr.sin_family = AF_INET;
+  serverAddr.sin_port = htons(4444);
+  serverAddr.sin_addr.s_addr = inet_addr("127.0.0.1");
+
+  connect(clientSocket,(struct sockaddr*)&serverAddr,sizeof(serverAddr));
+  printf("Connected to server\n");
+
+  printf("\nEnter the filename: ");
+  scanf("%s",buffer);
+  send(clientSocket,buffer,strlen(buffer)+1,0);
+  
+  while(recv(clientSocket,buffer,sizeof(buffer),0) > 0){
+    printf("%s",buffer);
+  }
+  printf("\n");
+  
+  close(clientSocket);
+}
---
+++ TCP/FTP/server.c
@@ -0,0 +1,62 @@
+#include<stdio.h>
+#include<stdlib.h>
+#include<string.h>
+#include<sys/socket.h>
+#include<sys/types.h>
+#include<netinet/in.h>
+#include<arpa/inet.h>
+#include<unistd.h>
+
+void main(){
+  struct sockaddr_in serverAddr,clientAddr;
+  char buffer[1024];
+  
+  int serverSocket = socket(AF_INET, SOCK_STREAM, 0);
+  printf("Server socket created successfully\n");
+
+  serverAddr.sin_family = AF_INET;
+  serverAddr.sin_port = htons(4444);
+  serverAddr.sin_addr.s_addr = inet_addr("127.0.0.1");
+
+  bind(serverSocket,(struct sockaddr*)&serverAddr,sizeof(serverAddr));
+  printf("Bind to port number %d\n",ntohs(serverAddr.sin_port));
+
+  listen(serverSocket,6);
+  printf("Listening...\n\n");
+
+  int addrSize = sizeof(clientAddr);
+  while(1){
+    int newSocket = accept(serverSocket,(struct sockaddr*)&clientAddr,&addrSize);
+    int pid = fork();
+
+    if(pid < 0){
+      printf("\nFork error\n");
+    }else if(pid == 0){
+      close(serverSocket);
+      recv(newSocket,buffer,sizeof(buffer),0);
+      FILE *fp = fopen(buffer,"r");
+
+      if(fp){
+        sprintf(buffer,"Server PID: %d\n\nFile Content:\n",getpid());
+        send(newSocket,buffer,strlen(buffer)+1,0);
+
+        while(fgets(buffer,sizeof(buffer),fp) != NULL){
+          send(newSocket,buffer,strlen(buffer)+1,0);
+        }
+
+        fclose(fp);
+      }else{
+        sprintf(buffer,"Server PID: %d\nFile Not Found",getpid());
+        send(newSocket,buffer,strlen(buffer)+1,0);
+      }
+
+      close(newSocket);
+      exit(0);
+    }else{
+      printf("Client requesting for file\n");
+      close(newSocket);
+    }
+  }
+
+  close(serverSocket);
+}
